af8c6fcc0a551cfacae41e73ea2acc159d11d66a
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-03 09:37:25 -0300

    :sparkles: implemented SignUP page

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 import GlobalStyles from './styles/global'
 
 import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 const App: React.FC = () => (
   <>
-    <Login />
+    <SignUp />
     <GlobalStyles />
   </>
 )
