31574c63adb3f951cb81a8f4bba96c4f1094024a
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-03 18:59:19 -0300

    :zap: configure Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,9 @@
 // eslint-disable-next-line no-unused-vars
-import React, { InputHTMLAttributes } from 'react'
+import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback } from 'react'
 // eslint-disable-next-line no-unused-vars
 import { StyledIconProps } from '@styled-icons/styled-icon'
+
+import { useField } from '@unform/core'
 
 import * as S from './styled'
 
@@ -10,10 +12,45 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon: React.ComponentType<StyledIconProps>
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <S.InputWrapper>
-    {Icon && <Icon size={16} />}
-    <S.Content {...rest} />
-  </S.InputWrapper>)
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const [isFocus, setIsFocus] = useState(false)
+  const [isField, setIsField] = useState(false)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  const { fieldName, defaultValue, error, registerField } = useField(name)
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocus(true)
+  }, [])
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocus(false)
+
+    setIsField(!!inputRef.current?.value)
+  }, [])
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value'
+    })
+  }, [fieldName, registerField])
+
+  return (
+    <S.InputWrapper isFocused={isFocus} isField={isField} >
+      {Icon && <Icon size={16} />}
+      <S.Content
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+
+      {error}
+    </S.InputWrapper>
+  )
+}
 
 export default Input
