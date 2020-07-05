7134b815299079af67772eb77c76744230caf8a3
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-05 09:45:08 -0300

    :sparkles: implemented Toast

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,7 +6,8 @@ import * as Yup from 'yup'
 
 import getValidationErrors from '../../utils/getValidationErrors'
 
-import { useAuth } from '../../hooks/AuthContext'
+import { useAuth } from '../../hooks/auth'
+import { useToast } from '../../hooks/toast'
 
 import logoImg from '../../assets/logo.svg'
 
@@ -24,6 +25,7 @@ const Login: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
   const { signIn } = useAuth()
+  const { addToast } = useToast()
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
@@ -36,15 +38,24 @@ const Login: React.FC = () => {
 
       await schema.validate(data, { abortEarly: false })
 
-      signIn({
+      await signIn({
         email: data.email,
         password: data.password
       })
     } catch (err) {
-      const errors = getValidationErrors(err)
-      formRef.current?.setErrors(errors)
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err)
+
+        formRef.current?.setErrors(errors)
+      }
+
+      addToast({
+        type: 'error',
+        title: 'Erro na autenticação',
+        description: 'Ocorreu um erro ao fazer login, cheque as credenciais'
+      })
     }
-  }, [signIn])
+  }, [signIn, addToast])
 
   return (
     <S.LoginWrapper>
