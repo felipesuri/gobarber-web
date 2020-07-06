c888bec0ac071a4dea645ff9b43d9a2124d41ec7
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-06 09:32:45 -0300

    :sparkles: implemented Route component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
+
+import Route from './Route'
 
 import Login from '../pages/Login'
 import SingUp from '../pages/SignUp'
+
+import Dashboard from '../pages/Dashboard'
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Login} />
     <Route path="/singup" exact component={SingUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 )
 
