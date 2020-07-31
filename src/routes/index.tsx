3ac5561ac4b477c4288b8168db299ffaffa4e38f
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-31 16:17:31 -0300

    :zap: add route for forgot-password

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import Route from './Route'
 
 import Login from '../pages/Login'
 import SingUp from '../pages/SignUp'
+import ForgotPassword from '../pages/ForgotPassword'
 
 import Dashboard from '../pages/Dashboard'
 
@@ -12,6 +13,7 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Login} />
     <Route path="/singup" exact component={SingUp} />
+    <Route path="/forgot-password" exact component={ForgotPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
