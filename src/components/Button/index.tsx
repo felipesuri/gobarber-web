6237d546e367f99ec65f751cb9d7794460912605
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-31 16:15:51 -0300

    :sparkles: add loading

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,15 @@
-// eslint-disable-next-line no-unused-vars
 import React, { ButtonHTMLAttributes } from 'react'
 
 import * as S from './styled'
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: number
+}
 
-const Button: React.FC<ButtonProps> = (props) => (
-  <S.ButtonWrapper type="button" {...props}>{props.children}</S.ButtonWrapper>
+const Button: React.FC<ButtonProps> = props => (
+  <S.ButtonWrapper type="button" {...props}>
+    {props.loading ? 'Carregando...' : props.children}
+  </S.ButtonWrapper>
 )
 
 export default Button
