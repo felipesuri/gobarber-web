ce84cad2313639abefd346e17e5fb88d18cd4853
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-31 17:16:28 -0300

    :truck: create route for ResetPassword

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,14 +6,16 @@ import Route from './Route'
 import Login from '../pages/Login'
 import SingUp from '../pages/SignUp'
 import ForgotPassword from '../pages/ForgotPassword'
+import ResetPassword from '../pages/ResetPassword'
 
 import Dashboard from '../pages/Dashboard'
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Login} />
-    <Route path="/singup" exact component={SingUp} />
-    <Route path="/forgot-password" exact component={ForgotPassword} />
+    <Route path="/singup" component={SingUp} />
+    <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
