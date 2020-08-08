87f2cb76ee664a4c53738a7024c92dfd4ee494b4
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-08 08:00:25 -0300

    :lipstick: create Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
 
-const Dashboard: React.FC = () => (
-  <h1>Dashboard</h1>
-)
+import { useAuth } from '../../hooks/auth'
+
+import logoImg from '../../assets/logo.svg'
+
+import * as S from './styled'
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth()
+
+  return (
+    <S.DashboardWrapper>
+      <S.Header>
+        <S.HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <S.Profile>
+            {user.avatar_url && <img src={user.avatar_url} alt={user.name} />}
+
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </S.Profile>
+
+          <button type="button" onClick={signOut}>
+            <S.LogoutIcon />
+          </button>
+        </S.HeaderContent>
+      </S.Header>
+    </S.DashboardWrapper>
+  )
+}
 
 export default Dashboard
