9fe8b1e523b0b0f31933d717887e63f7f838f1e2
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-09 07:33:22 -0300

    :lipstick: use Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,36 +1,18 @@
 import React, { useState } from 'react'
 
 import { useAuth } from '../../hooks/auth'
-
-import logoImg from '../../assets/logo.svg'
+import Header from '../../components/Header'
 
 import * as S from './styled'
 
 const Dashboard: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(new Date())
 
-  const { signOut, user } = useAuth()
+  const { user } = useAuth()
 
   return (
     <S.DashboardWrapper>
-      <S.Header>
-        <S.HeaderContent>
-          <img src={logoImg} alt="GoBarber" />
-
-          <S.Profile>
-            {user.avatar_url && <img src={user.avatar_url} alt={user.name} />}
-
-            <div>
-              <span>Bem-vindo,</span>
-              <strong>{user.name}</strong>
-            </div>
-          </S.Profile>
-
-          <button type="button" onClick={signOut}>
-            <S.LogoutIcon />
-          </button>
-        </S.HeaderContent>
-      </S.Header>
+      <Header user={user} />
 
       <S.Content>
         <S.Schedule>
--- src/pages/Dashboard/styled.ts
+++ src/pages/Dashboard/styled.ts
@@ -1,68 +1,7 @@
 import styled from 'styled-components'
-import { Power, Clock } from 'styled-icons/feather'
+import { Clock } from 'styled-icons/feather'
 
 export const DashboardWrapper = styled.div``
-
-export const Header = styled.div`
-  padding: 32px 0;
-  background: #28282e;
-`
-
-export const HeaderContent = styled.div`
-  max-width: 1120px;
-  margin: 0 auto;
-  display: flex;
-  align-items: center;
-  height: 60px;
-
-  > img {
-    height: 80px;
-  }
-
-  button {
-    margin-left: auto;
-    background: transparent;
-    border: 0;
-
-    svg {
-      color: #999591;
-      width: 20px;
-      height: 20px;
-    }
-  }
-`
-
-export const Profile = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 80px;
-
-  img {
-    width: 56px;
-    height: 56px;
-    border-radius: 50%;
-  }
-
-  div {
-    display: flex;
-    flex-direction: column;
-    margin-left: 16px;
-    line-height: 24px;
-
-    span {
-      color: #f4edf8;
-    }
-
-    strong {
-      color: #ff9000;
-    }
-  }
-`
-
-export const LogoutIcon = styled(Power)`
-  width: 20px;
-  height: 20px;
-`
 
 export const Content = styled.main`
   max-width: 1120px;
